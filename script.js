605c5c73f704438831c061a7590eb4c9c7d7414c
Jona <mr-jona@hotmail.com>
2022-06-08 14:30:46 +0200

    add list switcher and remove token button

--- script.js
+++ script.js
@@ -65,39 +65,64 @@ function addToken(event) {
     const tokenObject = JSON.parse(request.response).find(token => {
         return token.name === tokenName;
     });
-    
+
     localStorage.setItem(tokenName, JSON.stringify(tokenObject));
+
+    alert("Token added to your list!");
+};
+
+function loadMyList() {
+    container.innerHTML = "";
+        
+    let myArray = [];
+
+    for (let i = 0; i < localStorage.length; i++) {
+        myArray.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+    };
+
+    myArray.forEach((token) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const h1 = document.createElement('h1');
+        h1.textContent = token.name;
+        h1.style.backgroundImage = `url(${token.image})`;
+        // console.log(token);
+
+        const rank = document.createElement('p');
+        rank.textContent = `#${token.market_cap_rank}`;
+
+        const price = document.createElement('p');
+        price.textContent = `\$${token.current_price}`;
+
+        const btn = document.createElement('button');
+        btn.id = 'add';
+        btn.textContent = "X";
+
+        container.appendChild(card);
+        card.appendChild(h1);
+        card.appendChild(rank);
+        card.appendChild(price);
+        card.appendChild(btn);       
+    });
 };
 
 function switchList() {
+    
+    if (container.childNodes.length === 100) {
+        loadMyList();        
+    } else {
+        container.innerHTML = "";
+        request.onload();
+    };   
+};
+
+function removeToken(event) {
     container.innerHTML = "";
+    const tokenName = event.target.parentElement.firstElementChild.textContent;
 
-    //USE LOCALSTORAGE.KEY
-    // localStorage.forEach((token, index) => {
-    //     const card = document.createElement('div');
-    //     card.classList.add('card');
-
-    //     const h1 = document.createElement('h1');
-    //     h1.textContent = token.name;
-    //     h1.style.backgroundImage = `url(${token.image})`;
-    //     // console.log(token);
-
-    //     const rank = document.createElement('p');
-    //     rank.textContent = `#${index+1}`;
-
-    //     const price = document.createElement('p');
-    //     price.textContent = `\$${token.current_price}`;
-
-    //     const btn = document.createElement('button');
-    //     btn.id = 'add';
-    //     btn.textContent = "+";
-
-    //     container.appendChild(card);
-    //     card.appendChild(h1);
-    //     card.appendChild(rank);
-    //     card.appendChild(price);
-    //     card.appendChild(btn);       
-// });
+    localStorage.removeItem(tokenName);
+    loadMyList();
 };
 
 
@@ -109,9 +134,11 @@ const clickFunction = (e) => {
 
     if (e.target.textContent === "My List") {
         switchList();
-    }
+    };
 
-    console.log(localStorage);
+    if (e.target.textContent === "X") {
+        removeToken(e);
+    };
 };
 
 document.addEventListener('click', clickFunction);
